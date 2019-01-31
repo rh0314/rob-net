c9c108a8cade4d45acd5281e9336ca1d0b588fca
rh0314 <robharrison314@gmail.com>
2019-01-31 13:43:03 -0800

    configuring for heroku

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/rob-net'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/rob-net/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+console.log('server.js:  listening on port ' + process.env.PORT || 8080);
 app.listen(process.env.PORT || 8080);
