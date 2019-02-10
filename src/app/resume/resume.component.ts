a63c05229c755f2ffa07a0936565e91459dd30d1
rh0314 <robharrison314@gmail.com>
2019-02-10 04:30:20 +0000

    style updates

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
@@ -11,16 +12,42 @@ export class ResumeComponent implements OnInit {
   isLoaded: boolean = false;
   pdfViewer: Element;
   showAll: true;
+  zoomLevel: number = 1;
+  zbt = 115;
+  zbl: number;
 
   ngOnInit() {
+    window.addEventListener('resize', e => {
+      this.moveButtons();
+    });
+    window.addEventListener('scroll', e => {
+      this.moveButtons();
+    });
+    this.moveButtons();
+  }
 
+  moveButtons() {
+    const btns = document.getElementById('zoomBtnHolder');
+    const anchor = document.getElementById('anchor');
+    const am = anchor ? anchor.getBoundingClientRect().right - ( (anchor.getBoundingClientRect().right - anchor.getBoundingClientRect().left) / 2) : 0;
+    const bw = btns ? btns.getBoundingClientRect().width : 0;
+    this.zbl = am - (bw * .5);
+    if (btns) {
+      btns.style.top = this.zbt + 'px';
+      btns.style.left = this.zbl + 'px';
+    }
+    console.log('btns', btns.getBoundingClientRect());
+  }
+
+  zoomUp(up: boolean) {
+    this.zoomLevel = this.zoomLevel + .1 * (up ? 1 : -1);
   }
 
   afterLoadComplete(pdfData: any) {
     console.log('pdfData', pdfData);
     this.totalPages = pdfData.numPages;
     this.isLoaded = true;
-    
+
   }
 
   nextPage() {
