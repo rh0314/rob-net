8a3920943a82bb81eda2e1763070d0e158ac65f4
rh0314 <robharrison314@gmail.com>
2019-02-12 23:20:05 +0000

    ok moving on now, really, I mean it

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -57,14 +57,14 @@ export class HomeComponent implements OnInit {
 
   initItems() {
     this.techItems = [
-      { text: "Bootstrap", image: { path: "../../assets/images/internet-icons/bootstrap.png" }, startX: '9999', startY: '-999', gridCol: -1, gridRow: '0', duration: 10000, delay: 1000, backImage: '' },
+      { text: "Bootstrap", image: { path: "../../assets/images/internet-icons/bootstrap.png", sizeX: 1.5, sizeY: 1.5 }, startX: '9999', startY: '-999', gridCol: -1, gridRow: '0', duration: 10000, delay: 1000, backImage: '' },
       { text: "Database Driven Applications", image: { path: "../../assets/images/internet-icons/application-database-blk.png" }, startX: 8000, startY: 2000, gridCol: -1, gridRow: 1, duration: 0, delay: 0, backImage: '' },
       // { text: "Web-Based Tools", image: { path: "" }, startX: '', startY: '', gridCol: '', gridRow: '', duration: 0, delay: 0, backImage: '' },
       { text: "Delightful User Experience", image: { path: "../../assets/images/internet-icons/ux.png", sizeX: 1, sizeY: 1 }, startX: -1500, startY: -500, gridCol: 0, gridRow: 1, duration: 6000, delay: 2000, backImage: '' },
       { text: "Data transformation", image: { path: "../../assets/images/internet-icons/data-transformation.png" }, startX: '-300', startY: '-9000', gridCol: -1, gridRow: 2, duration: 2000, delay: 3500, backImage: '' },
-      { text: "ASP.NET", image: { path: "../../assets/images/aspnet-logo.png", sizeX: '1.1', sizeY: '0.6' }, startX: '6000', startY: '4000', gridCol: 2, gridRow: 0, duration: 3000, delay: 2500, backImage: '' },
+      { text: "ASP.NET", image: { path: "../../assets/images/net-logo.png", sizeX: '1.1', sizeY: '0.6' }, startX: '6000', startY: '4000', gridCol: 2, gridRow: 0, duration: 3000, delay: 2500, backImage: '' },
       { text: "C#", image: { path: "../../assets/images/internet-icons/c-sharp.png" }, startX: '650', startY: '-1000', gridCol: 1, gridRow: 1, duration: 0, delay: 0, backImage: '' },
-      { text: "Angular (2+)", image: { path: "../../assets/images/internet-icons/angular2.png", sizeX: 1.6, sizeY: 1.8 }, startX: 0, startY: 5000, gridCol: 0, gridRow: 0, duration: 1750, delay: 500, backImage: '' },
+      { text: "Angular (2+)", image: { path: "../../assets/images/internet-icons/angular2.png", sizeX: 1.7, sizeY: 1.7 }, startX: 0, startY: 5000, gridCol: 0, gridRow: 0, duration: 1750, delay: 500, backImage: '' },
       // { text: "AngularJS", image: { path: "../../assets/images/internet-icons/angularjs.png", sizeX: '250px', sizeY: 'auto' }, startX: '', startY: '', gridCol: '', gridRow: '', duration: 0, delay: 0, backImage: '' },
       { text: "ReactJS", image: { path: "../../assets/images/internet-icons/react-neg.v2.png", sizeX: '1.15', sizeY: 'auto' }, startX: 400, startY: -500, gridCol: 2, gridRow: 2, duration: 1000, delay: 2000, backImage: '' },
       { text: "JQuery", image: { path: "../../assets/images/internet-icons/jquery.png" }, startX: -1000, startY: 9999, gridCol: -2, gridRow: 1, duration: 4000, delay: 500, backImage: '' },
