f508e928c4ff22074a489e93222ef28dea6b9cc9
robharrison-ad <app.developer@mail.com>
2019-02-20 06:10:28 +0000

    intro page work

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { delay } from 'q';
 
 @Component({
   selector: 'app-intro',
@@ -16,12 +17,28 @@ export class IntroComponent implements OnInit {
   clickEnter(page) {
     const intro = document.getElementById('intro');
     if (intro) {
-      intro.style.transition = 'transform 750ms ease-in-out';
-      intro.style.transform += 'translateY(-1000px)';
-  }
+      this.transitionIt(intro);
+    }
     else {
       console.error('intro NOT FOUND!');
     }
+  }
+
+  transitionIt(el) {
+    el.style.transition = 'transform 3s ease-out';
+    el.style.transform = 'rotate(720deg) scale(0, 0)'
+    // setTimeout(() => {
+    //   el.style.transition = 'transform 1s ease-in';
+    //   el.style.transform = 'translate-y -1000px';
+
+    //   // el.style.transition = 'transform 3s ease-in';
+    //   // el.style.transform += 'scale(0.1, 0.1)';
+    //   // setTimeout(() => {
+    //   //   el.style.transition = 'transform 1s ease-in-out';
+    //   //   el.style.transform += 'translateY(-1000px)';
+    //   // }, 850);
+    // }, 2000);
+
   }
 
 }
