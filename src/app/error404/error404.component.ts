abca87f9ad270fb8a277f8d7b2798a9c240ada02
robharrison-ad <app.developer@mail.com>
2019-03-03 20:21:22 +0000

    err404

--- src/app/error404/error404.component.ts
+++ src/app/error404/error404.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { getNativeByTNode } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-error404',
@@ -13,13 +14,29 @@ export class Error404Component implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-  //   let p: Element | HTMLElement | HTMLCollectionOf<Element> = document.getElementsByClassName('pacman-container');
-  //   if (p && p[0]) {
-  //     p = p[0];
-  //     setTimeout(() => {
-  //       p.classList.add('move')
-  //     }, 500);
-  //   }
+    this.checkPacman();
+    //   let p: Element | HTMLElement | HTMLCollectionOf<Element> = document.getElementsByClassName('pacman-container');
+    //   if (p && p[0]) {
+    //     p = p[0];
+    //     setTimeout(() => {
+    //       p.classList.add('move')
+    //     }, 500);
+    //   }
   }
 
+  checkPacman() {
+    setTimeout(() => {
+      let p = document.getElementById('pacman');
+      const b = document.getElementById('homeBtn');
+      if (p && b)  {
+        if (p.getBoundingClientRect().left >= ((window.innerWidth / 2) - 250)) {
+          b.classList.add('eaten');
+        }
+        else {
+          b.classList.remove('eaten');
+        }
+      }
+      this.checkPacman();
+    }, 500);
+  }
 }
