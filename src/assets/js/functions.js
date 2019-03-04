4c43b7cc54847b53ffd9d0508619f859df4c1c21
robharrison-ad <app.developer@mail.com>
2019-03-04 19:29:54 +0000

    heroku update

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,27 +1,33 @@
 (() => {
-
+  console.log('functions.js loading...');
   let count = 0;
   let loading = true;
 
-  document.onload(() => {
-    var imgA = new Image();
-    count++;
-    imgA.onload = () => {
-      checkOff();
-    };
-    imgA.src = 'assets/images/coding-4-flip-2a.png';
+  // document.onload = () => {
+  //   console.log('document.onload');
+  //   var imgA = new Image();
+  //   count++;
+  //   imgA.onload = () => {
+  //     checkOff();
+  //   };
+  //   imgA.src = 'assets/images/coding-4-flip-2a.png';
 
-    var imgB = new Image();
-    count++;
-    imgB.onload = () => {
-      checkOff();
-    };
-    imgB.src = 'assets/images/coding-4-flip-2a.png';
+  //   var imgB = new Image();
+  //   count++;
+  //   imgB.onload = () => {
+  //     checkOff();
+  //   };
+  //   imgB.src = 'assets/images/coding-4-flip-2a.png';
+
+  //   var imgC = new Image();
+  //   count++;
+  //   imgC.onload = () => {
+  //     checkOff();
+  //   };
+  //   imgC.src = 'assets/images/blue-grid-paper-background-millimeter.jpg';
 
 
-
-
-  });
+  // };
 
 
   function checkOff() {
