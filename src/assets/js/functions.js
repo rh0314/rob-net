6be83f6ae77cafd6fdbcea9ac7a9c16da51224b3
robharrison-ad <app.developer@mail.com>
2019-03-05 00:46:46 +0000

    checkin

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -3,31 +3,31 @@
   let count = 0;
   let loading = true;
 
-  // document.onload = () => {
-  //   console.log('document.onload');
-  //   var imgA = new Image();
-  //   count++;
-  //   imgA.onload = () => {
-  //     checkOff();
-  //   };
-  //   imgA.src = 'assets/images/coding-4-flip-2a.png';
+  document.onload = () => {
+    console.log('document.onload');
+    var imgA = new Image();
+    count++;
+    imgA.onload = () => {
+      checkOff();
+    };
+    imgA.src = 'assets/images/coding-4-flip-2a.png';
 
-  //   var imgB = new Image();
-  //   count++;
-  //   imgB.onload = () => {
-  //     checkOff();
-  //   };
-  //   imgB.src = 'assets/images/coding-4-flip-2a.png';
+    var imgB = new Image();
+    count++;
+    imgB.onload = () => {
+      checkOff();
+    };
+    imgB.src = 'assets/images/coding-4-flip-2a.png';
 
-  //   var imgC = new Image();
-  //   count++;
-  //   imgC.onload = () => {
-  //     checkOff();
-  //   };
-  //   imgC.src = 'assets/images/blue-grid-paper-background-millimeter.jpg';
+    var imgC = new Image();
+    count++;
+    imgC.onload = () => {
+      checkOff();
+    };
+    imgC.src = 'assets/images/blue-grid-paper-background-millimeter.jpg';
 
 
-  // };
+  };
 
 
   function checkOff() {
